551cfb19327e631afc4061aa773109ff7953e3d3
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-10-21 11:33:41 +0200

    fix

--- send.js
+++ send.js
@@ -1,18 +1,44 @@
 const { EventHubProducerClient } = require("@azure/event-hubs");
 
-const connectionString = "EVENT HUBS NAMESPACE CONNECTION STRING";
-const eventHubName = "EVENT HUB NAME";
+const connectionString = "Endpoint=sb://pagopa-d-evh-ns01.servicebus.windows.net/;SharedAccessKeyName=";
+const eventHubName = "nodo-dei-pagamenti-biz-evt";
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
 
 async function main() {
 
     // Create a producer client to send messages to the event hub.
     const producer = new EventHubProducerClient(connectionString, eventHubName);
 
+    const newBtzEvt = {
+        id: makeid(10),
+        debtorPosition : {
+          modelType: "2",
+          noticeNumber : "302040000097735020584",
+          iuv: "97735020584"
+        },
+        creditor : {
+          idPA: "77777777777",
+          idBrokerPA: "77777777777",
+          idStation: "77777777777_01",
+          companyName: "company EC"
+        }
+    }
+
     // Prepare a batch of three events.
     const batch = await producer.createBatch();
-    batch.tryAdd({ body: "First event" });
-    batch.tryAdd({ body: "Second event" });
-    batch.tryAdd({ body: "Third event" });
+    batch.tryAdd({ body: newBtzEvt } );
+    // batch.tryAdd({ body: "Second event" });
+    // batch.tryAdd({ body: "Third event" });
 
     // Send the batch to the event hub.
     await producer.sendBatch(batch);
@@ -20,7 +46,7 @@ async function main() {
     // Close the producer client.
     await producer.close();
 
-    console.log("A batch of three events have been sent to the event hub");
+    console.log("A batch of events have been sent to the event hub");
 }
 
 main().catch((err) => {
